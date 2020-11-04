8580b38b760658fc54eb479975be670fe9e0a6c9
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-11-04 15:22:28 +0100

    add function to delete numbers

--- main.cpp
+++ main.cpp
@@ -6,17 +6,8 @@
 
 using namespace std;
 
-vector<string> getLine(string str)
+vector<string> getLine(string &str)
 {
-
-    // delete all operation on file in function getLine()
-
-    ofstream file;
-
-    string name = "new.txt";
-
-    file.open(name, ios::app);
-
     vector<string> words;
 
     string word = "";
@@ -24,7 +15,6 @@ vector<string> getLine(string str)
     {
         if (x == ' ')
         {
-            file << word << "\n";
             words.push_back(word);
             word = "";
         }
@@ -33,39 +23,59 @@ vector<string> getLine(string str)
             word = word + x;
         }
     }
-    file << word << "\n";
     words.push_back(word);
-    file.close();
     return words;
 }
 
-vector<double> sortingNumbers(vector<double> tmpVector){
-    for (int i = 1; i < tmpVector.size(); i++){
+vector<double> sortingNumbers(vector<double> &tmpVector)
+{
+    for (int i = 1; i < tmpVector.size(); i++)
+    {
         int j = i;
-       while (j > 0){
-           if (tmpVector[j] < tmpVector[j-1]){
-            double tmp = tmpVector[j];
-            tmpVector[j] = tmpVector[j-1];
-            tmpVector[j-1] = tmp;
-           }
-           j--;
+        while (j > 0)
+        {
+            if (tmpVector[j] < tmpVector[j - 1])
+            {
+                double tmp = tmpVector[j];
+                tmpVector[j] = tmpVector[j - 1];
+                tmpVector[j - 1] = tmp;
+            }
+            j--;
         }
     }
 
     return tmpVector;
 }
 
-vector<double> removeNumber(vector<double> tmpVector, int number){
+vector<double> removeNumber(vector<double> &tmpVector, double &number)
+{
 
     cout << number << endl;
-    // replace the number with last element after that pop back last element that is the number
+    double tmp = -1;
+    for (int i = 0; i < tmpVector.size(); i++)
+    {
+        if (number == tmpVector[i])
+        {
+            tmp = i;
+        }
+    }
+    if (tmp >= 0)
+    {
+
+        double help = tmpVector.back();
+        tmpVector.back() = tmpVector[tmp];
+        tmpVector[tmp] = help;
+        tmpVector.pop_back();
+    }
+
+    sortingNumbers(tmpVector);
 
     return tmpVector;
 }
 int main()
 {
 
-    ifstream input("lorem.txt");
+    ifstream input("InputFile.txt");
 
     vector<double> numbers;
 
@@ -86,18 +96,19 @@ int main()
             ++line;
         }
     }
-    
+
     for (int i = 0; i < jobs.size(); i++)
     {
         if (jobs[i][0] == "add")
         {
+            // add numbers to row
             for (int j = 1; j < jobs[i].size(); j++)
             {
                 double tmp = stod(jobs[i][j]);
                 numbers.push_back(tmp);
             }
             numbers = sortingNumbers(numbers);
-            cout << endl; 
+            cout << endl;
         }
         else if (jobs[i][0] == "remove")
         {
@@ -128,9 +139,8 @@ int main()
                     print << numbers[i] << "\n";
                 }
                 print.close();
-                
             }
-            else if(jobs[i].size() == 2)
+            else if (jobs[i].size() == 2)
             {
                 // create file (name.txt) where name is variable clean file add sorted numbers
                 string name = jobs[i][1];
@@ -148,30 +158,26 @@ int main()
                     print << numbers[i] << "\n";
                 }
                 print.close();
-                 
             }
         }
         else if (jobs[i][0] == "print" && jobs[i].size() == 1)
-            {
-                // only print numbers in cmd
+        {
+            // only print numbers in cmd
 
-                cout << "just print" << endl;
-                for (int i = 0; i < numbers.size(); i++)
+            cout << "just print" << endl;
+            for (int i = 0; i < numbers.size(); i++)
+            {
+                if (i == numbers.size() - 1)
                 {
-                   if (i == numbers.size()-1)
-                   {
-                        cout << numbers[i];
-                   }
-                   else
-                   {
-                        cout << numbers[i] << " ; ";
-                   }
-                   
-                   
+                    cout << numbers[i];
                 }
-                cout << endl;
-                
+                else
+                {
+                    cout << numbers[i] << " ; ";
+                }
             }
+            cout << endl;
+        }
         else if (jobs[i][0] == "graph" && jobs[i].size() != 1)
         {
             if (jobs[i][1] == "+")
@@ -179,19 +185,17 @@ int main()
                 // add graoh to file (name.txt) where name is variable just add not clean file
                 string name = jobs[i][2];
                 cout << "add graph to file " << name << endl;
-                
             }
-            else if(jobs[i].size() == 2)
+            else if (jobs[i].size() == 2)
             {
                 // create file (name.txt) where name is variable clean file add graph
                 string name = jobs[i][1];
                 cout << "add graph to file cleane file " << name << endl;
-                
             }
         }
         else if (jobs[i][0] == "graph" && jobs[i].size() == 1)
         {
-            // show graph in cmd 
+            // show graph in cmd
             cout << "graph" << endl;
         }
         else
@@ -200,9 +204,6 @@ int main()
             cout << " No comend like this: " << jobs[i][0] << endl;
         }
     }
-    
-    
-
 
     return 0;
 }
